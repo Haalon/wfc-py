80dbc95b5ffc78c8abc3666ed5fa513b16c5064d
Haalon <Haalon@yandex.com>
2019-08-26 00:36:33 +0300

    Initial commit

---
+++ field.py
@@ -0,0 +1,234 @@
+import numpy as np
+from types import SimpleNamespace
+import random
+import math
+import time
+import collections
+
+from table import Table
+
+test=np.array([
+    ['X',' ',' ',' ',' ','X',' '],
+    ['X','X','X','X','X','X',' '],
+    [' ',' ','X',' ',' ','X',' '],
+    [' ',' ','X',' ',' ','X',' '],
+    ['X','X','X',' ',' ','X','X'],
+    [' ',' ','X',' ',' ','X',' '],
+    ['X','X','X',' ',' ','X',' ']])
+
+class Field:
+    def __init__(self, table, width, height, loop_x=True, loop_y=True, seed=None):
+        self.table = table
+        self.loop_x = loop_x
+        self.loop_y = loop_y
+
+        self.width = width
+        self.height = height
+
+        self.FMX = width if loop_x else width - table.N + 1
+        self.FMY = height if loop_y else height - table.N + 1
+
+        self.wave = np.full((self.FMY, self.FMX, self.table.T), True)
+        self.changes = np.full((self.FMY, self.FMX), False)
+
+        self.log_prob = np.full(self.table.T, 0)
+        for t in range(0, self.table.T):
+            self.log_prob[t] = math.log(self.table.stationary[t])
+
+        self.log_t = math.log(self.table.T)
+
+        self.observe_count = 0
+
+        # resulting matrix
+        self.observed = np.full((self.FMY, self.FMX), -1)
+        
+        self.count_prop_passes = 0
+        self.rng = random.Random()
+
+        if seed != None:
+            self.rng.seed(seed)
+        
+
+    def observe(self):
+        self.observe_count += 1
+        observed_min = 1e+3
+        observed_sum = 0
+        main_sum = 0
+        log_sum = 0
+        noise = 0
+        entropy = 0
+        
+        argminx = -1
+        argminy = -1
+        amount = None
+        w = []
+
+        for y in range(0, self.FMY):
+            for x in range(0, self.FMX):
+                w = self.wave[y][x]
+                amount = 0
+                observed_sum = 0
+                t = 0
+                while t < self.table.T:
+                    if w[t]:
+                        amount += 1
+                        observed_sum += self.table.stationary[t]
+                    t += 1
+                if 0 == observed_sum:
+                    return False  # contradiction
+                noise = 1e-6 * self.rng.random()
+                if 1 == amount:
+                    entropy = 0
+                # elif self.T == amount:
+                #     entropy = self.log_t
+                else:
+                    main_sum = 0
+                    log_sum = math.log(observed_sum)
+                    t = 0
+                    while t < self.table.T:
+                        if w[t]:
+                            main_sum += self.table.stationary[t] * self.log_prob[t]
+                        t += 1
+                    entropy = log_sum - main_sum / observed_sum
+                if entropy > 0 and (entropy + noise < observed_min):
+                    observed_min = entropy + noise
+                    argminx = x
+                    argminy = y
+
+        # No minimum entropy, so mark everything as being observed...
+        if (-1 == argminx) and (-1 == argminy):
+            for y in range(0, self.FMY):
+                for x in range(0, self.FMX):
+                    for t in range(0, self.table.T):
+                        if self.wave[y][x][t]:
+                            self.observed[y][x] = t
+                            break
+            return True
+        
+        # A minimum point has been found, so prep it for propogation...
+        distribution = [0 for _ in range(0, self.table.T)]
+        for t in range(0, self.table.T):
+            distribution[t] = self.table.stationary[t] if self.wave[argminy][argminx][t] else 0
+        r = StuffRandom(distribution, self.rng.random())
+        for t in range(0, self.table.T):
+            self.wave[argminy][argminx][t] = (t == r)
+        self.changes[argminy][argminx] = True
+        self.observed[argminy][argminx] = t
+        return None
+
+    def propagate(self):
+        change = False
+        b = False
+        
+        #x2 = None
+        #y2 = None
+        for y1 in range(0, self.FMY):
+            for x1 in range(0, self.FMX):
+                if (self.changes[y1][x1]):
+                    self.changes[y1][x1] = False
+                    dy = (0 - self.table.N) + 1
+                    while dy < self.table.N:
+                    #for dx in range(1 - self.N, self.N):
+                        dx = (0 - self.table.N) + 1
+                        while dx < self.table.N:
+                        #for dy in range(1 - self.N, self.N):
+                            y2 = y1 + dy
+                            if y2 < 0 and self.loop_y:
+                                y2 += self.FMY
+                            elif y2 >= self.FMY and self.loop_y:
+                                    y2 -= self.FMY
+                            else:
+                                pass
+
+                            x2 = x1 + dx
+                            if x2 < 0 and self.loop_x:
+                                x2 += self.FMX
+                            elif x2 >= self.FMX and self.loop_x:
+                                    x2 -= self.FMX
+                            else:
+                                pass
+                            
+                            w1 = self.wave[y1][x1]
+                            w2 = self.wave[y2][x2]
+                            
+                            p = self.table.propagator[(self.table.N - 1) - dy][(self.table.N - 1) - dx]
+                            
+                            for t2 in range(0, self.table.T):
+                                if (not w2[t2]):
+                                    pass
+                                else:
+                                    b = False
+                                    prop = p[t2]
+                                    #print("Prop: {0}".format(prop))
+                                    i_one = 0
+                                    while (i_one < len(prop)) and (False == b):
+                                        b = w1[prop[i_one]]
+                                        i_one += 1                                    
+                                    if False == b:
+                                        self.changes[y2][x2] = True
+                                        change = True
+                                        w2[t2] = False
+                            dx += 1
+                        dy += 1
+                                  
+        return change
+
+    def step(self):
+        res = self.observe()
+        if res != None:
+            return res
+
+        presult = True
+        while(presult):
+            presult = self.propagate()
+
+
+def draw(field, defval='@'):
+    res = np.full((field.height, field.width), defval)
+    for y in range(field.FMY):
+        for x in range(field.FMX):
+            if field.observed[y,x] >= 0:
+                for dy in range(field.table.N):
+                    for dx in range(field.table.N):
+                        y2, x2 = ((y+dy) % field.FMY, (x+dx) % field.FMX)
+                        ind = field.observed[y2,x2]
+                        res[y2,x2] = field.table.patterns[ind][dy,dx]
+
+    text = ''
+    for y in range(field.height):
+        for x in range(field.width):
+            text+= str(res[y,x])
+        text+='\n'
+
+    return text[0:-1]
+
+
+def StuffRandom(source_array, random_value):
+    a_sum = sum(source_array)
+    
+    if 0 == a_sum:
+        for j in range(0, len(source_array)):
+            source_array[j] = 1
+        a_sum = sum(source_array)
+    for j in range(0, len(source_array)):
+        source_array[j] /= a_sum
+    i = 0
+    x = 0
+    while (i < len(source_array)):
+        x += source_array[i]
+        if random_value <= x:
+            return i
+        i += 1
+    return 0
+
+def main(t, w=30, h=30):
+    f = Field(t, w, h)
+
+    val = f.step()
+    while val == None:
+        print(draw(f))
+        val = f.step()
+
+    print(draw(f))
+    print(val)
+    return f
---
+++ table.py
@@ -0,0 +1,110 @@
+import numpy as np
+from types import SimpleNamespace
+import math
+import time
+import collections
+from bidict import bidict
+
+test=np.array([
+    ['X',' ',' ',' ',' ','X',' '],
+    ['X','X','X','X','X','X',' '],
+    [' ',' ','X',' ',' ','X',' '],
+    [' ',' ','X',' ',' ','X',' '],
+    ['X','X','X',' ',' ','X','X'],
+    [' ',' ','X',' ',' ','X',' '],
+    ['X','X','X',' ',' ','X',' ']])
+
+def take2(matrix, y, x):
+    return matrix.take(y, mode ='wrap', axis=0).take(x, mode ='wrap', axis=1)
+
+def can_align(ps1, ps2, dy, dx):
+    my, mx = ps1.shape
+    ox = max(0, dx)
+    ex = min(mx, mx+dx)
+
+    oy = max(0, dy)
+    ey = min(my, my+dy)
+
+    base_inter = take2(ps1, range(oy,ey), range(ox,ex))
+    annex_inter = take2(ps2, range(-ey, -oy), range(-ex, -ox))
+
+    return np.array_equal(base_inter, annex_inter)
+
+class Table:
+    def __init__(self, matrix, N_value = 3, loop_x = True, loop_y = True, symmetry_value = 8):
+        self.N = N_value
+        self.loop_y = loop_x
+        self.loop_y = loop_y
+        self.sym = symmetry_value        
+
+        # bi-directional map of values found in input matrix to positional indexes
+        self.values_map = bidict()
+
+        h, w = matrix.shape        
+
+        for y in range(h):
+            for x in range(w):
+                if not matrix[y][x] in self.values_map:
+                    self.values_map[matrix[y][x]] = len(self.values_map)
+
+        self.value_num = len(self.values_map)
+
+        y_range = h if loop_y else h - N_value + 1
+        x_range = w if loop_x else w - N_value + 1        
+
+        self.weights = collections.Counter()
+        ordering = []
+
+        for y in range(y_range):
+            for x in range(x_range):
+                ps = [0 for _ in range(8)]
+                ps[0] = take2(matrix, range(y , y + N_value),  range(x, x + N_value))
+                ps[1] = np.fliplr(ps[0])
+                ps[2] = np.rot90(ps[0])
+                ps[3] = np.fliplr(ps[2])
+                ps[4] = np.rot90(ps[2])
+                ps[5] = np.fliplr(ps[4])
+                ps[6] = np.rot90(ps[4])
+                ps[7] = np.fliplr(ps[6])
+                for s in range(0, self.sym):
+                    key = self.pattern2Key(ps[s])                   
+                    self.weights[key] += 1
+                    if not key in ordering:
+                        ordering.append(key)
+
+        # number of unique patterns
+        self.T = len(self.weights)              
+
+        self.patterns = np.full(self.T, None)
+        self.stationary = np.full(self.T, 0)
+
+        counter = 0
+        for key in ordering:
+            self.patterns[counter] = self.key2Pattern(key)
+            self.stationary[counter] = self.weights[key]
+            counter += 1
+
+
+        self.propagator = np.full( (2 * self.N - 1, 2 * self.N - 1, self.T), None)
+
+        for y in range(0, 2 * self.N - 1):
+            for x in range(0, 2 * self.N - 1):                                  
+                for t in range(0, self.T):
+                    a_list = []
+                    for t2 in range(0, self.T):
+                        if can_align(self.patterns[t], self.patterns[t2], y - self.N + 1, x - self.N + 1):
+                            a_list.append(t2)
+                    self.propagator[y][x][t] = [0 for _ in range(len(a_list))]
+                    for c in range(0, len(a_list)):
+                        self.propagator[y][x][t][c] = a_list[c]
+
+
+    def pattern2Key(self, p):
+        translated = np.array([self.values_map[val] for val in p.flatten()])
+        return np.array2string(translated, separator=' ')[1:-1]
+
+    def key2Pattern(self, key):
+        raw = np.fromstring(key, sep = ' ')
+        return np.array([self.values_map.inverse[key] for key in raw]).reshape(self.N, self.N)
+
+
